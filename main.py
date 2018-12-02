c5edaeeb72a54a6bc362bc1093f59deab9231dd6
Wykthor <wykthor.g@wgvale.com>
2018-12-02 12:05:56 -0300

    reforma nacasa

---
+++ main.py
@@ -0,0 +1,24 @@
+#imports
+#imports#
+
+#variables#
+#variables
+
+#classes#
+class user:
+	def __init__(self,)
+class group
+
+class users
+#classes
+
+#functions#
+#functions
+
+#main#
+def main(*args, **kwargs):
+	return None
+#main
+
+if(__name__ == "__main__"):
+	main()
